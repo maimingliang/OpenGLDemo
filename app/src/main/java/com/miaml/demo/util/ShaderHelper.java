c5fd671ee3913b63b47d48afb6d856be2c3acee8
麦明亮 <maimingliang8@163.com>
2017-08-20 09:53:36 +0800

    Update ShaderHelper.java

--- app/src/main/java/com/miaml/demo/util/ShaderHelper.java
+++ app/src/main/java/com/miaml/demo/util/ShaderHelper.java
@@ -6,10 +6,6 @@ import android.util.Log;
 /**
  * 类       名:
  * 说       明:
- * 修 改 记 录:
- * 版 权 所 有:   Copyright © 2017
- * 公       司:   深圳市旅联网络科技有限公司
- * version   0.1
  * date   2017/8/15
  * author   maimingliang
  */
