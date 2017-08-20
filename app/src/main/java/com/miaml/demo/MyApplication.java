796418bd8e37167a8444001c3a909af3c86bf66d
麦明亮 <maimingliang8@163.com>
2017-08-20 09:53:03 +0800

    Update MyApplication.java

--- app/src/main/java/com/miaml/demo/MyApplication.java
+++ app/src/main/java/com/miaml/demo/MyApplication.java
@@ -10,9 +10,7 @@ import com.github.hiteshsondhi88.libffmpeg.exceptions.FFmpegNotSupportedExceptio
 /**
  * 类       名:
  * 说       明:
- * 修 改 记 录:
- * 版 权 所 有:   Copyright © 2017
- * 公       司:   深圳市旅联网络科技有限公司
+
  * version   0.1
  * date   2017/8/5
  * author   maimingliang
