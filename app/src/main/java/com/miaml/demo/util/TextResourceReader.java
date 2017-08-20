e0bbd09a33030c140f24dba00e9f3bea83cca391
麦明亮 <maimingliang8@163.com>
2017-08-20 09:53:56 +0800

    Update TextResourceReader.java

--- app/src/main/java/com/miaml/demo/util/TextResourceReader.java
+++ app/src/main/java/com/miaml/demo/util/TextResourceReader.java
@@ -8,10 +8,7 @@ import java.io.InputStreamReader;
 
 /**
  * 类       名:
- * 说       明:
- * 修 改 记 录:
- * 版 权 所 有:   Copyright © 2017
- * 公       司:   深圳市旅联网络科技有限公司
+
  * version   0.1
  * date   2017/8/15
  * author   maimingliang
