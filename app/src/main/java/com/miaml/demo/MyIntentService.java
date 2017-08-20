37293ea9279c395af49c64bd2c3e594f5549ec38
麦明亮 <maimingliang8@163.com>
2017-08-20 09:53:18 +0800

    Update MyIntentService.java

--- app/src/main/java/com/miaml/demo/MyIntentService.java
+++ app/src/main/java/com/miaml/demo/MyIntentService.java
@@ -7,9 +7,6 @@ import android.text.TextUtils;
 /**
  * 类       名:
  * 说       明:
- * 修 改 记 录:
- * 版 权 所 有:   Copyright © 2017
- * 公       司:   深圳市旅联网络科技有限公司
  * version   0.1
  * date   2017/8/7
  * author   maimingliang
